f1e16ca5e93e43019a2420a859254a739e9869c4
Anubhav <anubhavpalway99@gmail.com>
2020-01-12 04:44:05 +0530

    posts done

--- backend/posts/controllers/post.controller.js
+++ backend/posts/controllers/post.controller.js
@@ -1,11 +1,17 @@
 const Post = require('../models/post.models.js');
+var url = require('url');
+var fs = require('fs');
 
 exports.create = (req, res) => {
+    var pic = "";
+    if (typeof req.file !== 'undefined') {
+        pic = req.file.filename;
+    }
     const post = new Post({
         username : req.body.username,
         title : req.body.title,
         text : req.body.text,
-        picture : req.file.filename,
+        picture : pic,
         comments : [],
         upvote : 0,
         downvote : 0,
@@ -22,13 +28,41 @@ exports.create = (req, res) => {
 
 };
 
-// exports.findOne = (req, res) => {
-//     User.findOne({username : req.params.userName}, "username email firstName lastName")
-//     .then(data => {
-//         res.send(data);
-//     }).catch(err => {
-//         res.status(500).send({
-//             message : err.message || "Some error occured while retrieving user",
-//         })
-//     });
-// };
+exports.getImage = (req, res) => {
+    // var pic = req.url.split('/').splice(-1)[0];
+    var pic = req.params.imageId;
+    console.log(pic)
+    fs.readFile('../assets/images/' + pic, (err, content) => {
+        if (err) {
+            res.writeHead(400, {'content-type': 'text/html'});
+            console.log(err);
+            res.end('No such post');
+        }
+        else {
+            res.writeHead(200, {'content-type': 'image/jpg'});
+            res.end(content);
+        }
+    })
+}
+
+exports.getPost = (req, res) => {
+    Post.findById(req.params.postId)
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message : err.message || "Some error occured while retrieving user",
+        })
+    });
+};
+
+exports.getPostIds = (req, res) => {
+    Post.find({}).sort({updatedAt: -1})
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message : err.message || "Some error occured while retrieving user",
+        })
+    });
+};
--- backend/posts/routes/post.routes.js
+++ backend/posts/routes/post.routes.js
@@ -4,5 +4,7 @@ const upload = multer({dest: '../assets/images'});
 module.exports = (app) => {
     const posts = require('../controllers/post.controller.js');
     app.post('/post', upload.single('image'), posts.create);
-    // app.get('/post/:postId', posts.findOne);
+    app.get('/image/:imageId', posts.getImage);
+    app.get('/post/:postId', posts.getPost)
+    app.get('/posts', posts.getPostIds)
 }
