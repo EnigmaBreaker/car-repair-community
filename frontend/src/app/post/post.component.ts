600e2f150ea65b9a47489dd05ce63a1167fb7809
aniket8200 <cs5160398@cse.iitd.ac.in>
2020-01-13 01:53:12 +0530

    Post list and post connected. Post request done in loop

--- frontend/src/app/post/post.component.ts
+++ frontend/src/app/post/post.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { PostService } from '../post.service';
+import { CommunicationService } from '../communication.service';
 
 @Component({
   selector: 'app-post',
@@ -7,10 +8,28 @@ import { PostService } from '../post.service';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
-  
-  constructor(private postService : PostService) { }
+  @Input() postIndex: number;
+  isImage = false;
+  isPost = false;
+  imageUrl = "";
+  post =  {};
+  constructor(private postService : PostService, private communicationService : CommunicationService) { }
 
   ngOnInit() {
+    this.communicationService.getPostInfo(this.postService.posts[this.postIndex]._id).subscribe(data => {
+      if(data.picture !== ""){
+        this.communicationService.getImage(data.picture).subscribe(blob => {
+          var url= window.URL.createObjectURL(blob);
+          console.log(url);
+          console.log(blob);
+          this.imageUrl = url;
+          this.isImage = true;
+        })
+      }
+      this.post = data;
+      this.isPost = true;
+      console.log(this.post);
+    });
   }
 
 }
--- frontend/src/app/postlist/postlist.component.ts
+++ frontend/src/app/postlist/postlist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { PostService } from '../post.service';
 import { CommunicationService } from '../communication.service';
 
@@ -8,12 +8,18 @@ import { CommunicationService } from '../communication.service';
   styleUrls: ['./postlist.component.css']
 })
 export class PostlistComponent implements OnInit {
-
-  constructor(private postService : PostService, private communicationService : CommunicationService) { }
+  numposts = 0;
+  showPosts = false;
+  constructor(private postService: PostService, private communicationService : CommunicationService) { }
 
   ngOnInit() {
-    this.communicationService.getAllPost().subscribe(data => this.postService.posts = data);
-    console.log(this.postService.posts);
-  }
+    this.communicationService.getAllPost()
+      .subscribe(data => {
+        this.postService.posts = data
+        // console.log(this.postService.posts);
+        this.numposts = this.postService.posts.length;
+        this.showPosts = true; 
+      });
+    }
 
 }
