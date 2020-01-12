c2df8305b71e73445d8457a6295a3705c2116b5e
aniket8200 <cs5160398@cse.iitd.ac.in>
2020-01-12 05:41:02 +0530

    Mongoose Visualizer added

--- backend/users/models/user.model.js
+++ backend/users/models/user.model.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
 
 const UserSchema = mongoose.Schema({
-    email : { type: String, unique: true },
-    username : { type: String, unique: true },
-    password : String,
-    firstName : String,
-    lastName : String,
+    email : { type: String, required: true, unique: true, index: true},
+    username : { type: String, required: true, unique: true, index: true},
+    password : {type: String, required: true},
+    firstName : {type: String, required: true},
+    lastName : {type: String, required: true},
     posts : [String],
 }, {
     timestamps : true
 });
 
+UserSchema.plugin(uniqueValidator);
 module.exports = mongoose.model('User', UserSchema);
